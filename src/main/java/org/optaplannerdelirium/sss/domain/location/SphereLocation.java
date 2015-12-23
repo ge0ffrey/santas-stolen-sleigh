403aeb6c1a44bc9ed74968e0757c713ff0ccfe7a
Lukáš Petrovický <lpetrovi@redhat.com>
2015-12-23 12:03:19 +0100

    Precalculate the distances instead of caching them
    This performs on constant time even with millions and millions of distances.

--- src/main/java/org/optaplannerdelirium/sss/domain/location/SphereLocation.java
+++ src/main/java/org/optaplannerdelirium/sss/domain/location/SphereLocation.java
@@ -18,25 +18,11 @@ package org.optaplannerdelirium.sss.domain.location;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @XStreamAlias("SphereLocation")
 public class SphereLocation extends Location {
 
-    private static final double EARTH_R_IN_KM = 6371;
-
-    private static double getDistanceToUncached(Location first, Location other) {
-        double latitudeDiff = Math.toRadians(other.getLatitude() - first.getLatitude());
-        double longitudeDiff = Math.toRadians(other.getLongitude() - first.getLongitude());
-        double a = Math.pow(Math.sin(latitudeDiff / 2), 2)
-                + Math.pow(Math.sin(longitudeDiff / 2), 2)
-                * Math.cos(Math.toRadians(first.getLatitude())) * Math.cos(Math.toRadians(other.getLatitude()));
-        double c = 2 * Math.asin(Math.sqrt(a));
-        return EARTH_R_IN_KM * c;
-    }
-
-    private static Map<Long, Map<Long, Double>> locationCache = new HashMap<Long, Map<Long, Double>>();
+    private static final int EARTH_R_IN_KM = 6371;
+    private static final int TEMP = 2 * EARTH_R_IN_KM;
 
     public SphereLocation() {
     }
@@ -46,29 +32,12 @@ public class SphereLocation extends Location {
     }
 
     @Override
-    public double getDistanceTo(Location other) {
-        if (this == other) {
-            return 0; // no need to cache this
-        }
-        // this should guarantee that no pair will be in the map twice
-        final long firstId = Math.min(this.getId(), other.getId());
-        final long secondId = Math.max(this.getId(), other.getId());
-        synchronized (locationCache) {
-            Map<Long, Double> cache = locationCache.get(firstId);
-            if (cache == null) { // create mapping for the first key
-                cache = new HashMap<Long, Double>();
-                locationCache.put(firstId, cache);
-            } else {
-                final Double result = cache.get(secondId);
-                if (result != null) { // the distance is cached already
-                    return result;
-                }
-            }
-            // cache the distance
-            final double result = getDistanceToUncached(this, other);
-            cache.put(secondId, result);
-            return result;
-        }
+    public double getDistanceTo(final Location other) {
+        final double dX = this.cartesianX - other.cartesianX;
+        final double dY = this.cartesianY - other.cartesianY;
+        final double dZ = this.cartesianZ - other.cartesianZ;
+        final double r = Math.sqrt(dX * dX + dY * dY + dZ * dZ);
+        return TEMP * Math.asin(r);
     }
 
 }
